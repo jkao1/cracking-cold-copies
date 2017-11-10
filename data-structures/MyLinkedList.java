aeacac27d4d9a9c910eaca351c3510dd5a448c61
Jason Kao <jasonkao85@gmail.com>
2017-11-09 22:33:20 -0500

    fixed toString, tested.

--- data-structures/MyLinkedList.java
+++ data-structures/MyLinkedList.java
@@ -1,13 +1,16 @@
 public class MyLinkedList<T> {
     private Node<T> head;
+    private int size;
 
     public MyLinkedList() {
         head = null;
+        size = 0;
     }
 
     public boolean add(T value) {
         if (head == null) {
             head = new Node<T>(value);
+            size++;
             return true;
         }
         Node<T> tracker = head;
@@ -15,6 +18,7 @@ public class MyLinkedList<T> {
             tracker = tracker.next;
         }
         tracker.next = new Node<T>(value);
+        size++;
         return true;
     }
 
@@ -24,10 +28,15 @@ public class MyLinkedList<T> {
         }
         String output = "[";
         Node<T> tracker = head;
-        while (tracker.next != null) {
+        while (tracker != null) {
             output += tracker.getValue() + ", ";
+            tracker = tracker.next;
         }
         return output.substring(0, output.length() - 2) + "]";
+    }
+
+    public int size() {
+        return size;
     }
 
     private class Node<T> {
