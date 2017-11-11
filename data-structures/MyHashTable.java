7a70d52999cee51e88efb31527b27ecde608cd7c
Jason Kao <jasonkao85@gmail.com>
2017-11-10 22:29:36 -0500

    Entry.java

--- data-structures/MyHashTable.java
+++ data-structures/MyHashTable.java
@@ -1,9 +1,35 @@
-public class MyHashTable<T> {
-    private LinkedList<T>[] array;
+public class MyHashtable {
+    private LinkedList<Entry>[] array;
 
     public MyHashTable() {
-        array = new LinkedList<T>()[10];
+        array = new LinkedList<Entry>()[10];
     }
 
-    public boolean 
+    private int getHashCode(String key) {
+        return value.length()**2;
+    }
+
+    public boolean put(String key, String value)
+    {
+        int hashCode = getHashCode(key);
+        array[hashCode % array.length].add(new Entry(key, value));
+    }
+
+    public String toString()
+    {
+        String output = "";
+    }
+
+    private class Entry {
+        public final String key, value;
+
+        public Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String toString() {
+            return String.format("(%s, %s)", key, value);
+        }
+    }
 }
